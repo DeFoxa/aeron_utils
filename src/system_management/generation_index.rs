c4c7cd97828933fda8dfa01213f9c825902c9d41
Malaparte4 <Malaparte44@protonmail.com>
2024-06-24 10:51:48 -0500

    Iter, IntoIter, FromIter on GenerationalIndexArray

--- src/system_management/generation_index.rs
+++ src/system_management/generation_index.rs
@@ -22,7 +22,7 @@ impl GenerationalIndex {
 #[derive(Clone, Default)]
 pub struct GenerationalIndexArray<T>(Vec<Option<ArrayEntry<T>>>);
 
-pub struct GenerationalIncexArrayIter<'a, T: 'a>(
+pub struct GenerationalIndexArrayIter<'a, T: 'a>(
     iter::Enumerate<slice::Iter<'a, Option<ArrayEntry<T>>>>,
 );
 
@@ -232,6 +232,130 @@ impl<T> GenerationalIndexArray<T> {
                 *entry = None
             }
         }
+    }
+
+    pub fn filter_map<F: FnMut(GenerationalIndex, T) -> Option<T>>(&mut self, mut f: F) {
+        for i in 0..self.0.len() {
+            let entry = &mut self.0[i];
+
+            if let Some(e) = entry.take() {
+                let generation_index = GenerationalIndex {
+                    index: i,
+                    generation: e.generation,
+                };
+
+                if let Some(value) = f(generation_index, e.value) {
+                    *entry = Some(ArrayEntry {
+                        value,
+                        generation: generation_index.generation,
+                    })
+                }
+            }
+        }
+    }
+
+    pub fn iter<'a>(&'a self) -> GenerationalIndexArrayIter<'a, T> {
+        GenerationalIndexArrayIter(self.0.iter().enumerate())
+    }
+
+    pub fn iter_mut<'a>(&'a mut self) -> GenerationalIndexArrayIterMut<'a, T> {
+        GenerationalIndexArrayIterMut(self.0.iter_mut().enumerate())
+    }
+}
+
+impl<'a, T: 'a> Iterator for GenerationalIndexArrayIter<'a, T> {
+    type Item = (GenerationalIndex, &'a T);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some((index, entry)) = self.0.next() {
+            if let &Some(ref entry) = entry {
+                return Some((
+                    GenerationalIndex {
+                        index,
+                        generation: entry.generation,
+                    },
+                    &entry.value,
+                ));
+            }
+        }
+        None
+    }
+}
+
+impl<'a, T: 'a> Iterator for GenerationalIndexArrayIterMut<'a, T> {
+    type Item = (GenerationalIndex, &'a mut T);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some((index, entry)) = self.0.next() {
+            if let &mut Some(ref mut entry) = entry {
+                return Some((
+                    GenerationalIndex {
+                        index,
+                        generation: entry.generation,
+                    },
+                    &mut entry.value,
+                ));
+            }
+        }
+        None
+    }
+}
+
+impl<T> Iterator for GenerationalIndexArrayIntoIter<T> {
+    type Item = (GenerationalIndex, T);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some((index, entry)) = self.0.next() {
+            if let Some(entry) = entry {
+                return Some((
+                    GenerationalIndex {
+                        index,
+                        generation: entry.generation,
+                    },
+                    entry.value,
+                ));
+            }
+        }
+        None
+    }
+}
+
+impl<'a, T: 'a> IntoIterator for &'a GenerationalIndexArray<T> {
+    type Item = (GenerationalIndex, &'a T);
+    type IntoIter = GenerationalIndexArrayIter<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
+    }
+}
+
+impl<'a, T: 'a> IntoIterator for &'a mut GenerationalIndexArray<T> {
+    type Item = (GenerationalIndex, &'a mut T);
+    type IntoIter = GenerationalIndexArrayIterMut<'a, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter_mut()
+    }
+}
+
+impl<T> IntoIterator for GenerationalIndexArray<T> {
+    type Item = (GenerationalIndex, T);
+    type IntoIter = GenerationalIndexArrayIntoIter<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        GenerationalIndexArrayIntoIter(self.0.into_iter().enumerate())
+    }
+}
+
+impl<T> FromIterator<(GenerationalIndex, T)> for GenerationalIndexArray<T> {
+    fn from_iter<I: IntoIterator<Item = (GenerationalIndex, T)>>(
+        iter: I,
+    ) -> GenerationalIndexArray<T> {
+        let mut map = GenerationalIndexArray::new();
+        for (entity, value) in iter {
+            map.insert(entity, value);
+        }
+        map
     }
 }
 
